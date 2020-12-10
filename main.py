caeaa2a4f862274998f05c79cf6fb9842b6f928c
Griot Nathan <nathan.griot@univ-avignon.fr>
2020-12-10 15:13:02 +0100

    added docstrings to current functions

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import argparse
 parser = argparse.ArgumentParser()
 #parser.add_argument("type", help = "read or write [ -r | -w ]")
 #parser.add_argument("datatype", help = "file or text [ -f | -t ]")
-parser.add_argument("-w", help="switch to writing mode")
+parser.add_argument("-w", help = "switch to writing mode")
 parser.add_argument("data", help = "file name or text", type = str )
 
 def text_to_bin(text):
+    ''' convert text into arrays of bins:
+        Input:
+            - text (str)
+
+        Ouput:
+            - Binary arrays
+    '''
     chars = bytearray(text, "utf8")
     bins = []
 
@@ -20,16 +27,33 @@ def text_to_bin(text):
     return bins
 
 def read(file):
+    ''' Read encrypted message in png image : 
+        Input:
+            - filename (str)
+        Output:
+            - Message (str)
+    '''
 
 
 def write(file,bins):
+    ''' Write ASCII message in png image :
+    input:
+        - filename (str)
+        - array of ASCII in bin 
+    output:
+        - True or False (if code was written)
+    
+    errors:
+        - code is to long too fit in image
+    '''
     f = png.Reader(file)
     print(f.read())
     flatImg = f.read_flat()
     
 def main():
+    ''' Main function '''
     args = parser.parse_args()
-    
+
     if args.w:
         bins = text_to_bin("test")
         write(args.data,bins)

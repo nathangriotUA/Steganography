81b969bae71a135ab7970a184ee89816d73effc7
Griot Nathan <nathan.griot@univ-avignon.fr>
2020-12-15 10:34:11 +0100

    Change low weight bit in writing mode

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 #parser.add_argument("type", help = "read or write [ -r | -w ]")
 #parser.add_argument("datatype", help = "file or text [ -f | -t ]")
-parser.add_argument("-w", help = "switch to writing mode")
+parser.add_argument("-w", help = "switch to writing mode", action = "store_true")
 parser.add_argument("data", help = "file name or text", type = str )
 
 def text_to_bin(text):
@@ -22,33 +22,63 @@ def text_to_bin(text):
     for char in chars:
         bin_char = bin(char)
         bin_char = bin_char[2:]
-        bins.append(bin_char)
+        bin_char = list(bin_char)
+        bins += bin_char
 
     return bins
 
-def read(file):
+def read(file_encrypted):
     ''' Read encrypted message in png image : 
         Input:
-            - filename (str)
+            - filename_encrypted (str)
         Output:
             - Message (str)
     '''
+    f1 = png.Reader(file_encrypted)
+
+    flatImg1 = f.read_flat()
 
 
 def write(file,bins):
     ''' Write ASCII message in png image :
-    input:
-        - filename (str)
-        - array of ASCII in bin 
-    output:
-        - True or False (if code was written)
-    
-    errors:
-        - code is to long too fit in image
+        input:
+            - filename (str)
+            - array of ASCII in bin 
+        output:
+            - True or False (if code was written)
+        
+        errors:
+            - code is to long too fit in image
     '''
+
     f = png.Reader(file)
-    print(f.read())
-    flatImg = f.read_flat()
+    f = f.asRGBA()
+    RGBA = True if f[3]["alpha"] else False
+    list_binaries_of_files = []
+    for x in f[2]:
+        list_binaries_of_files += list(x)
+
+    if len(list_binaries_of_files) < len(bins):
+        SystemError("Message is too long in order to fit in this image.")
+
+    for img_value, encrypted_value  in zip(list_binaries_of_files, bins):
+        bin_value = '{0:08b}'.format(img_value)
+        list_bins_values = list(bin_value)
+        
+        print(list_bins_values)
+        print(encrypted_value)
+        if encrypted_value == '1':
+            list_bins_values[7] = '1'
+        elif list_bins_values[7] == '1' and encrypted_value == '0':
+            list_bins_values[7] = '0'
+        
+        print(list_bins_values)
+        
+        break
+
+
+
+
     
 def main():
     ''' Main function '''
